94aa467e1dd437cf6b8e60e35c1eaf5a3ef3ece8
or berman <orber3@gmail.com>
2021-08-07 15:18:35 +0300

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         /learn react/i
+        react
         </p>
         <a
           className="App-link"
