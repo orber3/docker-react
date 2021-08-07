0e7953ff7e158687b6d53a3a2249da15ab6652f3
or berman <orber3@gmail.com>
2021-08-07 12:10:44 +0300

    git init

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+wwe44444444
         </p>
         <a
           className="App-link"
@@ -16,8 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+wwe</a>
       </header>
     </div>
   );
